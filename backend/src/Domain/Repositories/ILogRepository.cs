f49682e76ca307ced6efdd80ec5822cdd50a31fe
Alex Scigalszky <alexpablo.90@gmail.com>
2021-01-19 09:19:49 -0300

    Log entity and repository

---
+++ backend/src/Domain/Repositories/ILogRepository.cs
@@ -0,0 +1,11 @@
+﻿using Core.Repositories.Base;
+using Domain.Entities;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories
+{
+    public interface ILogRepository : IRepository<Log>
+    {
+        Task<Log> GetByIdAsync(long id);
+    }
+}
---
+++ backend/src/Domain/Repositories/Implementation/LogRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Data;
+using Domain.Entities;
+using Infrastructure.Repository.Base;
+
+namespace Domain.Repositories.Implementation
+{
+    public class LogRepository : Repository<Log>, ILogRepository
+    {
+        public LogRepository(ExampleContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
